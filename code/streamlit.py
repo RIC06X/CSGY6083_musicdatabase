790623e2b20f27edf2422dac4c477a849cef3501
Yanjie Xu <yx2304@nyu.edu>
2021-12-12 23:39:50 -0500

    Add title and fixed bugs

--- code/streamlit.py
+++ code/streamlit.py
@@ -1,3 +1,4 @@
+import datetime
 import pandas as pd
 import psycopg2
 import streamlit as st
@@ -34,6 +35,9 @@ def query_db(sql: str):
     df = pd.DataFrame(data=data, columns=column_names)
     return df
 
+"# CSGY6083 MusicDB"
+"by Hanqing Zhang(hz2758) Yanjie Xu(yx2304)"
+
 "## Browse tables"
 sql_all_table_names = "SELECT relname FROM pg_class WHERE relkind='r' AND relname !~ '^(pg_|sql_)';"
 try:
@@ -56,7 +60,7 @@ if table_name:
 
 
 
-"## Find all songs of an artist "
+"## Give out the information of songs produced by a selected artist "
 sql_all_artist = "SELECT DISTINCT(name) FROM artist;"
 try:
     artists = query_db(sql_all_artist)["name"].tolist()
@@ -67,32 +71,18 @@ except:
 if artist:
     f"Display the table"
 
-    sql_all= f"select song.name, song.duration_ms, song.release_date from artist, song_artist_map, song where artist.aid = song_artist_map.aid and song_artist_map.sid = song.sid and artist.name = '{artist}'; "
+    sql_all= f"""select song.name, song.duration_ms, song.release_date 
+                from artist, song_artist_map, song 
+                where artist.aid = song_artist_map.aid 
+                and song_artist_map.sid = song.sid 
+                and artist.name = '{artist}'
+                order by song.name; 
+                """
     try:
         df = query_db(sql_all)
         st.dataframe(df)
     except:
         st.write("Sorry! Something went wrong with your query, please try again.")
-
-
-"## select songs from a playlist given playlist name "
-sql_playlist = "select pname from playlist;"
-try:
-    data_rows = query_db(sql_playlist)["pname"].tolist()
-    select_data = st.selectbox("Choose a playlist", data_rows,1)
-except:
-    st.write("Sorry! Something went wrong with your query, please try again.")
-
-if select_data:
-    f"Display the table"
-
-    sql_all_songs= f"select song.name song_name, album.name album_name, song.duration_ms, song.release_date from playlist, playliststores, song, album where playlist.pid = playliststores.pid and song.sid = playliststores.sid and song.album_id = album.album_id and playlist.pname = '{select_data}' order by song.name;"
-    try:
-        df = query_db(sql_all_songs)
-        st.dataframe(df)
-    except:
-        st.write("Sorry! Something went wrong with your query, please try again.")
-
 
 "## Let the user search for an artist and the system can display the song and the album information corresponding to the searched artist"
 artist_searched = st.text_input('Type the artist name here:')
@@ -114,6 +104,34 @@ if artist_searched:
     except:
         st.write("Sorry! Something went wrong with your query, please try again.")
 
+
+"## Show the songs of a playlist by chosen playlist name "
+sql_playlist = "select pname from playlist;"
+try:
+    data_rows = query_db(sql_playlist)["pname"].tolist()
+    select_data = st.selectbox("Choose a playlist", data_rows,1)
+except:
+    st.write("Sorry! Something went wrong with your query, please try again.")
+
+if select_data:
+    f"Display the table"
+
+    sql_all_songs= f"""select song.name song_name, album.name album_name, song.duration_ms, song.release_date 
+                from playlist, playliststores, song, album 
+                where playlist.pid = playliststores.pid 
+                and song.sid = playliststores.sid 
+                and song.album_id = album.album_id 
+                and playlist.pname = '{select_data}' 
+                order by song.name;
+                """
+    try:
+        df = query_db(sql_all_songs)
+        st.dataframe(df)
+    except:
+        st.write("Sorry! Something went wrong with your query, please try again.")
+
+
+
 "## Find all artists that belongs to a certain company and show his/her songs count"
 sql_all_company = "select company.name from company;"
 try:
@@ -132,7 +150,9 @@ if select_data:
         and company.company_id = artistowned.company_id
         and song_artist_map.aid = artist.aid
         and company.name = '{select_data}'
-        group by company.name, artist.name;
+        group by company.name, artist.name
+        order by company_name
+        ;
     """
     try:
         df = query_db(sql_all_info)
@@ -161,23 +181,70 @@ if select_data:
         and song.album_id = album.album_id
         and playlist.pname = '{select_data}'
         and duration_ms < '{select_duration[1] * 1000}'
-        and duration_ms > '{select_duration[0] * 1000}';
+        and duration_ms > '{select_duration[0] * 1000}'
+        order by song_name
+        ;
     """
     try:
         df = query_db(sql_all_info)
-        st.dataframe(df.to_dict())
+        st.dataframe(df)
     except:
         st.write("Sorry! Something went wrong with your query, please try again.")
 
+"## Find total songs by company name given date range" 
 
-# "## Leave a review"
-# st.subheader("Leave a review please")
-# conduct_every_song= 'SELECT name FROM song;'
+"Please input/select a date range"
+start_date = st.date_input("start date", min_value = datetime.date(1950,1,1), max_value= datetime.date(2021,12,30))
+end_date = st.date_input("end date", min_value = datetime.date(1950,1,1), max_value= datetime.date(2021,12,31))
 
-# choose_song= st.selectbox('Please choose a song', conduct_every_song)
-# rating=st.selectbox('Rating',[1,2,3,4,5])
-# if st.button("Submit"):
-#     update_rating(rating,choose_song)
-#     st.success("Thank you for leaving a rating")
+if start_date and end_date and start_date < end_date:
+    start_date = start_date.strftime("%Y-%m-%d")
+    end_date = end_date.strftime("%Y-%m-%d")
+
+    f"Display the table"
+
+    sql_all_info= f"""
+        select company.name, count(*) total_songs
+        from song, company, artistowned, song_artist_map
+        where song_artist_map.sid = song.sid
+        and company.company_id = artistowned.company_id
+        and song_artist_map.aid = artistowned.aid
+        and release_date >= '{start_date}'
+        and release_date <= '{end_date}'
+        group by company.name
+        order by total_songs desc
+    """
+    try:
+        df = query_db(sql_all_info)
+        st.dataframe(df)
+    except:
+        st.write("Sorry! Something went wrong with your query, please try again.")
     
 
+"## Let the user choose a rating and the system will give out name of the songs based on ratings along with the user who give the rating"
+sql_all_ratings = "SELECT DISTINCT(rating) from rating;"
+
+try:
+    # ratings = query_db(sql_all_ratings)["rating"].tolist()
+    choose_rating = st.selectbox("Choose a rating", [1,2,3,4,5])
+except:
+    st.write("Sorry! Something went wrong with your query, please try again.")
+
+if select_data:
+    f"Display the table"
+
+    sql_recommend_songs= f"""
+        SELECT rating.rating , song.name song_name, _user.name user_name
+        FROM rating, song, ratingby,_user
+        WHERE rating.rating >= '{choose_rating}' 
+        AND rating.cid = ratingby.cid
+        AND song.sid = rating.sid
+        AND _user.user_id = ratingby.uid
+        ORDER BY rating desc
+        ;
+    """
+    try:
+        df = query_db(sql_recommend_songs)
+        st.dataframe(df)
+    except:
+        st.write("Sorry! Something went wrong with your query, please try again.")
